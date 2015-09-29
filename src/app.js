9e6fdcbd05cfa9482980f1badf538131ef5a3322
Robin Brandt <robinbrandt81@gmail.com>
2015-09-29 10:47:35 +0200

    Some setup and a filter added

---
+++ src/app.js
@@ -0,0 +1,15 @@
+angular.module('RobinsFilters', [])
+	.filter('reverse', function () {
+		return function (input) {
+			var i, out;
+			if (angular.isString(input)) {
+				out = '';
+				for (i = 0; i < input.length; i = i + 1) {
+					out = input.charAt(i) + out;
+				}
+				return out;
+			} else {
+				return input;
+			}
+		};
+	});
