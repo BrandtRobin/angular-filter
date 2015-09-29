4701d619379e9fcf13ee4df6e65f965e1b8c184e
Robin Brandt <robinbrandt81@gmail.com>
2015-09-29 12:06:24 +0200

    added first unit test

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-angular.module('RobinsFilters', [])
+angular.module('robinsFilters', [])
 	.filter('reverse', function () {
 		return function (input) {
 			var i, out;
 			if (angular.isString(input)) {
 				out = '';
 				for (i = 0; i < input.length; i = i + 1) {
-					out = input.charAt(i) + out;
+				out = input.charAt(i) + out;
 				}
 				return out;
 			} else {
